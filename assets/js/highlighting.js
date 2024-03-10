de5c8fee7440f23d1dc603e271a3fe83dfd4c948
Nathan Ng <nathanng@Nathans-MacBook-Pro-2.local>
2024-03-10 17:57:00 -0400

    Add hover highlighting

--- assets/js/highlighting.js
+++ assets/js/highlighting.js
@@ -9,9 +9,31 @@ console.log(contElements.length)
 
 document.addEventListener('DOMContentLoaded', function() {
   var genElements = document.getElementsByClassName('generation');
+  var contElements = document.getElementsByClassName('context');
+  for (var j = 0; j < contElements.length; j++) {
+    contElements[j].setAttribute('data-color', "white");
+  }
 
   for (var i = 0; i < genElements.length; i++) {
+
     genElements[i].addEventListener('click', function() {
+      for (var j = 0; j < genElements.length; j++) {
+        genElements[j].style.backgroundColor = "white";
+        genElements[j].setAttribute('data-color', "white");
+      }
+      this.style.backgroundColor = "#888";
+      this.setAttribute('data-color', "#888");
+
+      var index = Array.prototype.indexOf.call(genElements, this);
+
+      var contElements = document.getElementsByClassName('context');
+      for (var j = 0; j < contElements.length; j++) {
+        contElements[j].style.backgroundColor = scores[index][j];
+        contElements[j].setAttribute('data-color', scores[index][j]);
+      }
+    });
+
+    genElements[i].addEventListener('mouseover', function() {
       for (var j = 0; j < genElements.length; j++) {
         genElements[j].style.backgroundColor = "white";
       }
@@ -22,6 +44,17 @@ document.addEventListener('DOMContentLoaded', function() {
       var contElements = document.getElementsByClassName('context');
       for (var j = 0; j < contElements.length; j++) {
         contElements[j].style.backgroundColor = scores[index][j];
+      }
+    });
+
+    genElements[i].addEventListener('mouseout', function() {
+      for (var j = 0; j < genElements.length; j++) {
+        genElements[j].style.backgroundColor = genElements[j].getAttribute('data-color');
+      }
+
+      var contElements = document.getElementsByClassName('context');
+      for (var j = 0; j < contElements.length; j++) {
+        contElements[j].style.backgroundColor = contElements[j].getAttribute('data-color');
       }
     });
   }
